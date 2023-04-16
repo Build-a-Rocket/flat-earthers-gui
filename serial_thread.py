4ea4500822bde70aa78d2123cb6acfa41a223c18
huangxia000 <huangxia000@tamu.edu>
2023-04-15 22:16:30 -0500

    camera testing 1

--- serial_thread.py
+++ serial_thread.py
@@ -10,7 +10,7 @@ class SerialThread(QObject):
     readFailed = pyqtSignal(str)
     dataReceived = pyqtSignal(bytes)
 
-    def __init__(self, serial_instance, write_signal=None):
+    def __init__(self, serial_instance):
         """\
         Initialize thread.
         Note that the serial_instance' timeout is set to one second!
@@ -18,11 +18,8 @@ class SerialThread(QObject):
         """
         QObject.__init__(self)
         self.serial = serial_instance
-        serial_instance.close()
+        self.serial.close()
         self.alive = True
-
-        #self.writeSignal = write_signal
-        #self.writeSignal.connect(self.write)
 
     def stop(self):
         """Stop the reader thread"""
